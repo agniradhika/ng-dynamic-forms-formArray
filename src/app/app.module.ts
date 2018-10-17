03379b50ec9d07a300b604c678b6fad94c84acff
Radhika <rdkagnihotri@gmail.com>
2018-10-17 17:00:23 +0530

    import samplelib module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SampleLibComponent } from 'node_modules/sample-lib-1234';
+import { SampleLibModule } from 'node_modules/sample-lib-1234';
 import { ReactiveFormsModule } from "@angular/forms";
 import { DynamicFormsCoreModule } from "@ng-dynamic-forms/core";
 import { DynamicFormsBootstrapUIModule } from "@ng-dynamic-forms/ui-bootstrap";
 @NgModule({
   declarations: [
     AppComponent,
-    SampleLibComponent
   ],
   imports: [
     BrowserModule,
+    SampleLibModule,
     ReactiveFormsModule,
     DynamicFormsCoreModule.forRoot(),
     DynamicFormsBootstrapUIModule
